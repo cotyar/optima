d5f12d5a17677ab8a1bf15c9d1bad42fd1749982
Saddam Bilalov <saddam.bilalov@gmail.com>
2020-12-10 11:55:24 +0100

    feat: Created an initial version of the k8s deployment. - Removed the port assignment from the code base. - Improved lunch settings.

--- CSharp/Optima.Actors/Program.cs
+++ CSharp/Optima.Actors/Program.cs
@@ -1,14 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Text.Json;
 using Dapr.Actors.AspNetCore;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Optima.Actors.Actors;
 using Optima.Interfaces;
 
@@ -21,8 +13,6 @@ namespace Optima.Actors
             CreateHostBuilder(args).Build().Run();
         }
         
-        private const int AppChannelHttpPort = 3000;
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
@@ -37,22 +27,21 @@ namespace Optima.Actors
                             //     a.DrainOngoingCallTimeout = TimeSpan.FromSeconds(35);
                             //     a.DrainRebalancedActors = true;
                             // });
-                            
+
                             // Register actor types
                             actorRuntime.RegisterActor<DatasetEntryActor>();
                             actorRuntime.RegisterActor<DatasetRegistryActor>();
                             actorRuntime.RegisterActor<SecurityRegistryActor>();
                             actorRuntime.RegisterActor<DataProviderActor>();
                             actorRuntime.RegisterActor<DataProviderManagerActor>();
-                            
+
                             actorRuntime.ConfigureJsonSerializerOptions(options =>
                             {
                                 // options.PropertyNameCaseInsensitive = true;
                                 // options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
                                 options.Converters.Add(new ProtoMessageConverter());
                             });
-                        })
-                        .UseUrls($"http://localhost:{AppChannelHttpPort}/");
+                        });
                 });
     }
 }
